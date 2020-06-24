376b4dabea19f8171d147eb155f4dfbf6271996a
Tarik Guney <atarikguney@gmail.com>
2020-06-23 23:09:32 -0600

    Initial project structure

---
+++ CommandCore.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandCore.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
