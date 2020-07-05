49e3ba4951c11fed79ce6d15ff61a8e1a428b4cd
Tarik Guney <atarikguney@gmail.com>
2020-07-05 01:59:01 -0600

    started implementing the help creation functionlity

--- CommandCore.Library/Attributes/ParameterNameAttribute.cs
+++ CommandCore.Library/Attributes/ParameterNameAttribute.cs
@@ -2,10 +2,11 @@ using System;
 
 namespace CommandCore.Library.Attributes
 {
-    public class ParameterNameAttribute: Attribute
+    public class ParameterNameAttribute : Attribute
     {
         public string Name { get; }
         public string Alias { get; set; }
+        public string Description { get; set; }
 
         public ParameterNameAttribute(string name)
         {
--- CommandCore.Library/Attributes/VerbNameAttribute.cs
+++ CommandCore.Library/Attributes/VerbNameAttribute.cs
@@ -5,6 +5,7 @@ namespace CommandCore.Library.Attributes
     public class VerbNameAttribute: Attribute
     {
         public string Name { get; }
+        public string Description { get; set; }
 
         public VerbNameAttribute(string name)
         {
