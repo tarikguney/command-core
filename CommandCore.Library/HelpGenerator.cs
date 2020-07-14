403519975ba5991fedd8ba3670829799eb862942
Tarik Guney <atarikguney@gmail.com>
2020-07-13 20:37:43 -0600

    Fixed the help generator after adding multi command and options binding feature

--- CommandCore.Library/HelpGenerator.cs
+++ CommandCore.Library/HelpGenerator.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using CommandCore.Library.Attributes;
@@ -13,6 +14,7 @@ namespace CommandCore.Library
         {
             _verbTypeFinder = verbTypeFinder;
         }
+
         public StringBuilder Build()
         {
             var helpBuilder = new StringBuilder();
@@ -23,40 +25,59 @@ namespace CommandCore.Library
                 helpBuilder.Append("No help is found!");
                 return helpBuilder;
             }
-            
+
             helpBuilder.AppendLine("VERBS:");
             helpBuilder.AppendLine("----------------------");
             foreach (var verbType in allTypes)
             {
-                var attribute = verbType.GetCustomAttribute<VerbNameAttribute>();
-                var verbName = attribute?.Name ?? verbType.Name;
+                var attributes = verbType.GetCustomAttributes<VerbNameAttribute>().ToList();
+                var verbName = attributes.FirstOrDefault()?.Name ?? verbType.Name;
                 helpBuilder.Append(
                     $"{verbName}");
-                // If there is description to show for teh verb, show it after a colon.
-                if (!string.IsNullOrWhiteSpace(attribute?.Description))
+                if (attributes.Count > 1)
                 {
-                    helpBuilder.AppendLine($": {attribute?.Description}");
+                    var alternativeNames = attributes.Skip(1).Select(a => a.Name).Aggregate((a, b) => $"{a},{b}");
+                    helpBuilder.Append($" ({alternativeNames}) ");
                 }
+
+                // If there is description to show for teh verb, show it after a colon.
+                if (!string.IsNullOrWhiteSpace(attributes?.FirstOrDefault()?.Description))
+                {
+                    helpBuilder.AppendLine($": {attributes?.FirstOrDefault()?.Description}");
+                }
+
                 var optionProperties = verbType.BaseType!.GetGenericArguments()[0].GetProperties();
                 if (optionProperties.Length == 0)
                 {
                     continue;
                 }
+
                 helpBuilder.AppendLine($"  Options:");
-                foreach (var optionPropertyInfo in  optionProperties)
+                foreach (var optionPropertyInfo in optionProperties)
                 {
-                    var optionPropertyAttribute = optionPropertyInfo.GetCustomAttribute<OptionNameAttribute>();
-                    var optionName = optionPropertyAttribute?.Name ?? optionPropertyInfo.Name;
+                    var optionsAttributes = optionPropertyInfo.GetCustomAttributes<OptionNameAttribute>().ToList();
+                    var firstAttribute = optionsAttributes.FirstOrDefault();
+
+                    var optionName = firstAttribute?.Name ?? optionPropertyInfo.Name;
                     helpBuilder.Append(
                         $"  --{optionName}");
-                    if (!string.IsNullOrWhiteSpace(optionPropertyAttribute?.Alias))
+                    if (!string.IsNullOrWhiteSpace(firstAttribute?.Alias))
                     {
-                        helpBuilder.Append($" (-{optionPropertyAttribute!.Alias})");
+                        helpBuilder.Append($" -{firstAttribute!.Alias}");
                     }
 
-                    if (!string.IsNullOrWhiteSpace(optionPropertyAttribute?.Description))
+                    if (optionsAttributes.Count > 1)
                     {
-                        helpBuilder.AppendLine($": {optionPropertyAttribute!.Description}");
+                        var altOptionNames = optionsAttributes.Skip(1)
+                            .Select(a => "--" + a.Name + 
+                                         (string.IsNullOrWhiteSpace(a.Alias) ? "" : " -" + a.Alias))
+                            .Aggregate((a, b) => $"{a},{b}");
+                        helpBuilder.Append($" ({altOptionNames}) ");
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(firstAttribute?.Description))
+                    {
+                        helpBuilder.AppendLine($": {firstAttribute!.Description}");
                     }
                 }
             }
